820ba8e1a44af7d7f2b8e1b855479a0e40c6d6a1
Hershal Bhave <hershal.bhave@gmail.com>
2017-01-02 03:17:16 -0600

    got it

--- test/limit.js
+++ test/limit.js
@@ -9,40 +9,53 @@ class OperationQueue {
   constructor(concurrency) {
     this._started = false;
     this._concurrency = concurrency;
-    this._operations = new Array();
+    this._operationsInFlight = new Array();
+    this._pendingOperations = new Array();
+    this._finishedOperations = new Array();
+
+    this._resolveCallback = undefined;
+    this._rejectCallback = undefined;
   }
 
   addOperation(operation) {
-    this._operations.push(operation);
+    this._pendingOperations.push(operation);
   }
 
-  taskFinished(i) {
-    console.log('task finished ' + i + ' ' + this._operations[i].toString());
-    this._operations[i].finished = true;
+  taskFinished(task) {
+    let idx = this._operationsInFlight.indexOf(task);
+    assert(idx >= 0);
+    this._operationsInFlight.splice(idx, 1);
 
-    /* scan for next unstarted task */
-    const op = this._operations.filter((o) => !(o.started))[0];
-    const j = this._operations.indexOf(op);
-    op.start()
-      .then(() => {
-        this.taskFinished(j);
+    console.log('OQ: Operation Finished +++ ' + task.toString());
+    this._finishedOperations.push(task);
+
+    if (this._operationsInFlight.length == 0 && this._pendingOperations == 0) {
+      console.log('OQ: ALL DONE!');
+      this._resolveCallback();
+    }
+
+    this._start();
+  }
+
+  _start() {
+    while (this._operationsInFlight.length < this._concurrency) {
+      let op = this._pendingOperations.pop();
+      if (!op) {
+        break;
+      }
+
+      op.start().then(() => {
+        this.taskFinished(op);
       });
+      console.log('OQ: Operation Started --- ' + op.toString());
+      this._operationsInFlight.push(op);
+    }
   }
 
   start() {
-    this._started = true;
-    return new Promise((resolve, reject) => {
-      const numTasks = Math.min(this._concurrency, this._operations.length);
-
-      /* initial task start is a special case */
-      for (let i=0; i<numTasks; ++i) {
-        this._operations[i].start().then(() => {
-          this.taskFinished(i);
-        });
-      }
-
-      /*  */
-
+    return new Promise((res, rej) => {
+      this._resolveCallback = res;
+      this._start();
     });
   }
 }
@@ -51,18 +64,17 @@ class Operation {
   constructor(task) {
     this._task = task;
     this.started = false;
-    this.finished = false;
     this.cancelled = false;
   }
 
   /* make sure your task return a promise! */
   start() {
     this.started = true;
-    return this._task();
+    return new Promise((res, rej) => this._task(res));
   }
 
   toString() {
-    return `Operation: (s: ${this.started}, f: ${this.finished}, c: ${this.cancelled})`;
+    return `Operation: (started: ${this.started}, cancelled: ${this.cancelled})`;
   }
 }
 
@@ -70,28 +82,36 @@ describe('OperationQueue tests', function () {
 
   let queue, operations;
   const concurrency = 5;
+  const numOperations = 10;
 
   beforeEach(function () {
     queue = new OperationQueue(concurrency);
-    operations = Array.from(new Array(concurrency), (x, i) =>
-                            new Operation(() => {
-                              return new Promise(
-                                (resolve, reject) => {
-                                  console.log('task started ' + i);
-                                  const timeout = Math.random() * 1000;
-                                  setTimeout(() => {
-                                    console.log('task done: ' + i);
-                                    resolve();
-                                  }, timeout);
-                                });
-                            }));
+    operations = Array.from(new Array(numOperations), (x, i) => {
+      return new Operation((done)  => {
+        setTimeout(() => done(), Math.random()*500);
+      });
+    });
+  });
+
+  it('task should be valid', function (done) {
+    operations[0].start().then(() => done());
+  });
+
+  it(`should run ${concurrency} tasks`, function (done) {
+    operations
+      .slice(0, concurrency)
+      .forEach((t) => queue.addOperation(t));
+    queue
+      .start()
+      .then(() => done());
   });
 
   it('should run a task', function (done) {
     operations.forEach((t) => queue.addOperation(t));
-    queue.start().then(() => {
-      console.log('done!!');
-      done();
-    });
+    queue.start()
+      .then(() => {
+        console.log('done!!');
+        done();
+      });
   });
 });
