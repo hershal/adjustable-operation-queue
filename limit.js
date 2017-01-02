09a8d467c60f220edd9c05b14f357dfc0c96f393
Hershal Bhave <hershal.bhave@gmail.com>
2017-01-02 03:28:54 -0600

    rearrange code to its proper place

--- limit.js
+++ limit.js
@@ -1,7 +1,78 @@
 'use strict';
 
-/* returns a promise */
-function limit(num, tasks) {
+const assert = require('assert');
 
+class OperationQueue {
+  constructor(concurrency) {
+    this._started = false;
+    this._concurrency = concurrency;
+    this._operationsInFlight = new Array();
+    this._pendingOperations = new Array();
+    this._finishedOperations = new Array();
+
+    this._resolveCallback = undefined;
+    this._rejectCallback = undefined;
+  }
+
+  addOperation(operation) {
+    this._pendingOperations.push(operation);
+  }
+
+  start() {
+    return new Promise((res, rej) => {
+      this._resolveCallback = res;
+      this._start();
+    });
+  }
+
+  _taskFinished(task) {
+    let idx = this._operationsInFlight.indexOf(task);
+    assert(idx >= 0);
+    this._operationsInFlight.splice(idx, 1);
+
+    /* console.log('OQ: +++ Operation Finished ' + task.toString()); */
+    this._finishedOperations.push(task);
+
+    if (this._operationsInFlight.length == 0 && this._pendingOperations == 0) {
+      /* console.log('OQ: ALL DONE!'); */
+      this._resolveCallback();
+    }
+
+    this._start();
+  }
+
+  _start() {
+    while (this._operationsInFlight.length < this._concurrency) {
+      let op = this._pendingOperations.pop();
+      if (!op) {
+        break;
+      }
+
+      op.start().then(() => {
+        this._taskFinished(op);
+      });
+      /* console.log('OQ: --- Operation Started ' + op.toString()); */
+      this._operationsInFlight.push(op);
+    }
+  }
 }
-module.exports.limit = limit;
+module.exports.OperationQueue = OperationQueue;
+
+class Operation {
+  constructor(task) {
+    this._task = task;
+    this.started = false;
+    this.cancelled = false;
+  }
+
+  /* make sure your task return a promise! */
+  start() {
+    this.started = true;
+    return new Promise((res, rej) => this._task(res));
+  }
+
+  toString() {
+    return `Operation: (started: ${this.started}, cancelled: ${this.cancelled})`;
+  }
+}
+module.exports.Operation = Operation;
--- test/limit.js
+++ test/limit.js
@@ -1,80 +1,8 @@
 'use strict';
 
-const limit = require('../limit');
 const assert = require('assert');
+const {Operation, OperationQueue} = require('../limit');
 
-class OperationQueue {
-  constructor(concurrency) {
-    this._started = false;
-    this._concurrency = concurrency;
-    this._operationsInFlight = new Array();
-    this._pendingOperations = new Array();
-    this._finishedOperations = new Array();
-
-    this._resolveCallback = undefined;
-    this._rejectCallback = undefined;
-  }
-
-  addOperation(operation) {
-    this._pendingOperations.push(operation);
-  }
-
-  start() {
-    return new Promise((res, rej) => {
-      this._resolveCallback = res;
-      this._start();
-    });
-  }
-
-  _taskFinished(task) {
-    let idx = this._operationsInFlight.indexOf(task);
-    assert(idx >= 0);
-    this._operationsInFlight.splice(idx, 1);
-
-    /* console.log('OQ: +++ Operation Finished ' + task.toString()); */
-    this._finishedOperations.push(task);
-
-    if (this._operationsInFlight.length == 0 && this._pendingOperations == 0) {
-      /* console.log('OQ: ALL DONE!'); */
-      this._resolveCallback();
-    }
-
-    this._start();
-  }
-
-  _start() {
-    while (this._operationsInFlight.length < this._concurrency) {
-      let op = this._pendingOperations.pop();
-      if (!op) {
-        break;
-      }
-
-      op.start().then(() => {
-        this._taskFinished(op);
-      });
-      /* console.log('OQ: --- Operation Started ' + op.toString()); */
-      this._operationsInFlight.push(op);
-    }
-  }
-}
-
-class Operation {
-  constructor(task) {
-    this._task = task;
-    this.started = false;
-    this.cancelled = false;
-  }
-
-  /* make sure your task return a promise! */
-  start() {
-    this.started = true;
-    return new Promise((res, rej) => this._task(res));
-  }
-
-  toString() {
-    return `Operation: (started: ${this.started}, cancelled: ${this.cancelled})`;
-  }
-}
 
 describe('OperationQueue tests', function () {
   let queue, operations;
