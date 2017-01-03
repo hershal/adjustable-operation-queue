bee90285adebe04f88b6c645d861249e2379b22f
Hershal Bhave <hershal.bhave@gmail.com>
2017-01-02 22:20:33 -0600

    remove started from Operation

--- index.js
+++ index.js
@@ -69,12 +69,10 @@ module.exports.OperationQueue = OperationQueue;
 
 class Operation {
   get uid() { return this._uid; }
-  get started() { return this._started; }
 
   constructor(task) {
     this._task = task;
     this._uid = this._generateUID();
-    this._started = false;
   }
 
   _s4() {
@@ -87,12 +85,11 @@ class Operation {
 
   /* make sure your task resolves or rejects the promise! */
   start() {
-    this._started = true;
     return new Promise((res, rej) => this._task(res, rej));
   }
 
   toString() {
-    return `Operation: (guid: ${this.uid}, started: ${this._started})`;
+    return `Operation: (guid: ${this.uid})`;
   }
 }
 module.exports.Operation = Operation;
